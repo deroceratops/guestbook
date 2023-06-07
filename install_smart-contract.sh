82a157e0c645f93d860f8091a226951c9f94315e
deroceratops <deroceratops@proton.me>
2023-06-07 17:00:06 +0200

    Change indentation

--- install_smart-contract.sh
+++ install_smart-contract.sh
@@ -7,8 +7,8 @@ clear='\033[0m'
 TXID=$(curl --silent --request POST --data-binary @guestbook.bas http://127.0.0.1:30000/install_sc | sed 's/{"txid":"//;s/"}//')
 
 if [[ $TXID == "" ]]; then
-  printf "${red}Could not install the smart contract. Did you start the DERO simulator?\n${clear}"
-  exit 0
+    printf "${red}Could not install the smart contract. Did you start the DERO simulator?\n${clear}"
+    exit 0
 fi;
 
 OUTPUTFILES=("src/scid.js" "guestbook-service/scid.js")
@@ -16,12 +16,12 @@ OUTPUTFILES=("src/scid.js" "guestbook-service/scid.js")
 CONTENT="const scid = '$TXID'; export default scid;"
 
 for ((i = 0; i < ${#OUTPUTFILES[@]}; ++i)); do
-  OUTPUTFILE="${OUTPUTFILES[$i]}"
-  echo $CONTENT > $OUTPUTFILE
+    OUTPUTFILE="${OUTPUTFILES[$i]}"
+    echo $CONTENT > $OUTPUTFILE
 done
 
 printf "${green}The smart contract was installed.${clear}\n"
 printf "The TXID $TXID was saved to the following files:\n"
 for ((i = 0; i < ${#OUTPUTFILES[@]}; ++i)); do
-  echo "${OUTPUTFILES[$i]}"
+    echo "${OUTPUTFILES[$i]}"
 done
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,15 +1,17 @@
 const Messages = ({ messages }) => {
-  return (
-    <>
-      <ul>
-        {
-           messages.map((message, key) => (
-               <li key={key}>{message}</li>
-           ))
-        }
-      </ul>
-    </>
-  );
+    return (
+        <>
+            <ul>
+                {
+                    messages.map((message, key) => (
+                        <li key={key}>
+                            {message}
+                        </li>
+                    ))
+                }
+            </ul>
+        </>
+    );
 };
 
 export default Messages;
