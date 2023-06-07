3ba53636be8fd21810eaa4a2bdb6ccf06d9ef1e2
deroceratops <deroceratops@proton.me>
2023-06-07 20:45:51 +0200

    Fix code style with Eslint

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,17 +1,17 @@
-const Messages = ({ messages }) => {
-    return (
-        <>
-            <ul>
-                {
-                    messages.map((message, key) => (
-                        <li key={key}>
-                            {message}
-                        </li>
-                    ))
-                }
-            </ul>
-        </>
-    );
+const Messages = ({messages}) => {
+  return (
+    <>
+      <ul>
+        {
+          messages.map((message, key) => (
+            <li key={key}>
+              {message}
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  );
 };
 
 export default Messages;
